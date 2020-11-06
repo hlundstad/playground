218a15d97cb8fcf763a3a0a24f994fd2ba2a8eba
Hege Lundstad <>
2020-11-06 17:07:12 +0100

    Lagt til test

--- product-service/src/test/java/com/lundstad/products/WebLayerTest.java
+++ product-service/src/test/java/com/lundstad/products/WebLayerTest.java
@@ -6,6 +6,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -24,7 +25,7 @@ public class WebLayerTest {
     private ProductService service;
 
     @Test
-    public void greetingShouldReturnMessageFromService() throws Exception {
+    public void getOneProduct() throws Exception {
         Product ruleProduct = new Product(1,"eple",2D);
         when(service.getProduct(1)).thenReturn(ruleProduct);
         this.mockMvc.perform(get("/product/1"))
@@ -32,4 +33,17 @@ public class WebLayerTest {
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.name", is(ruleProduct.getName())));
     }
+
+    @Test
+    public void createOneProduct() throws Exception {
+        Product ruleProduct = new Product(1,"eple",2D);
+        when(service.getProduct(1)).thenReturn(ruleProduct);
+        this.mockMvc.perform(get("/product/1"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(ruleProduct.getName())));
+    }
+
+
+
 }
