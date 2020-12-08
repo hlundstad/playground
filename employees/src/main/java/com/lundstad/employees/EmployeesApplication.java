0e8db6a3f257b233e327b6a11ad35713edab4d09
Hege Lundstad <>
2020-12-08 15:50:04 +0100

    Lagt til glemte filer

---
+++ employees/src/main/java/com/lundstad/employees/EmployeesApplication.java
@@ -0,0 +1,26 @@
+package com.lundstad.employees;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
+
+@Log4j2
+@SpringBootApplication
+public class EmployeesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeesApplication.class, args);
+
+    }
+
+    @Bean
+    ApplicationRunner applicationRunner(Environment environment) {
+        return args -> {
+            log.info("message from application.properties " + environment.getProperty("message-from-application-properties"));
+        };
+    }
+
+}
