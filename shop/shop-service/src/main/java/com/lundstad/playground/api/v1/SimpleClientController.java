c54bc6a90b50b6694a94fe4dcbc8d9d5e8521780
hege <hlundstad@gmail.com>
2021-03-16 15:18:04 +0100

    Opprydning

--- shop/shop-service/src/main/java/com/lundstad/playground/api/v1/SimpleClientController.java
+++ shop/shop-service/src/main/java/com/lundstad/playground/api/v1/SimpleClientController.java
@@ -1,10 +1,7 @@
 package com.lundstad.playground.api.v1;
 
-
-import com.lundstad.playground.model.AirQuality;
 import com.lundstad.playground.model.Quote;
 import com.lundstad.playground.model.SimpleClientData;
-import com.lundstad.playground.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -29,19 +26,12 @@ public class SimpleClientController {
 
     @RequestMapping("/")
     public String index() {
-        return "Hello, the time at the server is now " + new Date() + "\n";
+        return "Hello, the time at the shop server is now " + new Date() + "\n";
     }
 
     @RequestMapping("/alive")
     public String alive() {
         return "Hello, I am alive!" ;
-    }
-
-    @RequestMapping("/quora")
-    public String quora() {
-        Quote quote = template.getForObject(
-                "https://freequote.herokuapp.com/", Quote.class);
-        return quote.getValue().toString();
     }
 
     @RequestMapping("/quora2")
@@ -50,23 +40,6 @@ public class SimpleClientController {
     }
 
 
-    @RequestMapping("/airquality")
-    public AirQuality[] airQuality() {
-        AirQuality[] airQuality = template.getForObject(
-                "https://api.met.no/weatherapi/airqualityforecast/0.1/areas?areaclass=fylke", AirQuality[].class);
-        for(AirQuality aq : airQuality){
-            System.out.println(aq.getName() + "::" );
-        }
-        return airQuality;
-    }
-
-    @RequestMapping("/user/{userId}")
-    public User user(@PathVariable String userId) {
-        User user = template.getForObject(
-                "https://gorest.co.in/public-api/users/"+ userId, User.class);
-        System.out.println(user.getData().toString() + "::" );
-        return user;
-    }
 
 
 
